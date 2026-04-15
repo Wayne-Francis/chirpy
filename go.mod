9d284a1d9c110bc0b12f5eb984a0734edfaf1dda
MrFaultyT <wanye787@gmail.com>
2026-04-15 21:24:02 +0100

    lesson 1 , created the server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Wayne-Francis/chirpy
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	mux.Handle("/api/", http.FileServer(http.Dir(".")))
+	err := srv.ListenAndServe()
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+}
