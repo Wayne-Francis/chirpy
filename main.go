682753a24e3bdbbf4741bea9dba79707f49513a7
MrFaultyT <wanye787@gmail.com>
2026-04-21 21:42:14 +0100

    finished the server chapter

--- main.go
+++ main.go
@@ -12,10 +12,19 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthzHandler)
 	err := srv.ListenAndServe()
 	if err != nil {
 		fmt.Printf("%v", err)
 		return
 	}
+}
+
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
 }
