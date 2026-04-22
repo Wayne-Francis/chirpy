a4497ac27aa49b76ad0107141c8880947c8a4ac3
MrFaultyT <wanye787@gmail.com>
2026-04-22 21:07:59 +0100

    added some stateful handlers

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
@@ -12,8 +13,11 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	apiCfg := &apiConfig{}
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 	err := srv.ListenAndServe()
 	if err != nil {
 		fmt.Printf("%v", err)
@@ -27,4 +31,26 @@ func healthzHandler(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	body := fmt.Sprintf("Hits: %v", hits)
+	w.Write([]byte(body))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
 }
