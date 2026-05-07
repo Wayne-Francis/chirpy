4a21e40676613db24a88ff8fc292734353b80297
MrFaultyT <wanye787@gmail.com>
2026-05-07 11:21:02 +0100

    adding sorting and calling chirps by id

--- handler_get_chirps.go
+++ handler_get_chirps.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/google/uuid"
@@ -15,10 +16,49 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 		Body      string    `json:"body"`
 		UserID    uuid.UUID `json:"user_id"`
 	}
+	s := r.URL.Query().Get("author_id")
+	if s != "" {
+		userID, err := uuid.Parse(s)
+		if err != nil {
+			respondWithError(w, 400, "Bad Request")
+			return
+		}
+		chirps, err := cfg.db.GetChirpsByAuthor(r.Context(), userID)
+		if err != nil {
+			respondWithError(w, 500, "Cannot Retrieve Chirps")
+			return
+		}
+		sortParam := "asc"
+		if r.URL.Query().Get("sort") == "desc" {
+			sortParam = "desc"
+		}
+		var response []responseBody
+		for _, chirp := range chirps {
+			response = append(response, responseBody{
+				ID:        chirp.ID,
+				CreatedAt: chirp.CreatedAt,
+				UpdatedAt: chirp.UpdatedAt,
+				Body:      chirp.Body,
+				UserID:    chirp.UserID,
+			})
+		}
+		sort.Slice(response, func(i, j int) bool {
+			if sortParam == "desc" {
+				return response[i].CreatedAt.After(response[j].CreatedAt)
+			}
+			return response[i].CreatedAt.Before(response[j].CreatedAt)
+		})
+		respondWithJSON(w, 200, response)
+		return
+	}
 	chirps, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
 		respondWithError(w, 500, "Cannot Retrieve Chirps")
 		return
+	}
+	sortParam := "asc"
+	if r.URL.Query().Get("sort") == "desc" {
+		sortParam = "desc"
 	}
 	var response []responseBody
 	for _, chirp := range chirps {
@@ -30,6 +70,12 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 			UserID:    chirp.UserID,
 		})
 	}
+	sort.Slice(response, func(i, j int) bool {
+		if sortParam == "desc" {
+			return response[i].CreatedAt.After(response[j].CreatedAt)
+		}
+		return response[i].CreatedAt.Before(response[j].CreatedAt)
+	})
 	respondWithJSON(w, 200, response)
 }
 
